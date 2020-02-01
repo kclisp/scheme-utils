68705af59cb882534b872225d39204ba233e05ab
kclisp <kenchen1368@gmail.com>
2020-02-01 12:02:08 -0500

    better loading

--- load.scm
+++ load.scm
@@ -1,2 +1,13 @@
-(load-at "generic-procs" "load")
-(load '("utils" "macro-utils" "test"))
+(define (at dir proc)
+  (let ((here (pwd)))
+    (dynamic-wind
+        (lambda () (cd dir))
+        proc
+        (lambda () (cd here)))))
+(define (load-at dir pathname . args)
+  (at dir
+      (lambda () (apply load pathname args))))
+(at "~/coding/scheme/utils"
+    (lambda ()
+      (load-at "generic-procs" "load")
+      (load '("utils" "macro-utils" "test"))))
